d134f766080028849bbc65a0f9d960dadbc3f84a
dsath <devinsather@live.com>
2017-06-11 14:02:18 -0700

    Order and restaurant select tabs working.

--- public/index.js
+++ public/index.js
@@ -2,25 +2,20 @@
 var placeOrder = document.getElementById("ord");
 var restaurants = document.getElementById("restaurants");
 var hallList = document.getElementsByClassName("navsub")[0];
+var orderBox = document.getElementById("order-box");
 hallList.style.display = "none";
+orderBox.style.display = "none";
 
-function revealOrderBox() {
-   var fields = document.getElementsByClassName("order-field");
-   for(var i = 0; i < fields.length; i++) {
-      	   fields[i].style.display = "block";
-   }
-}
-
-function revealRemHalls() {
-   if (hallList.style.display === "none") {
-	   hallList.style.display = "block";
+function revealRemove(ele) {
+   if (ele.style.display === "none") {
+	   ele.style.display = "block";
    }else{
-	   hallList.style.display = "none";
+	   ele.style.display = "none";
    }
 }
    	   
-placeOrder.onclick = function() {revealOrderBox()};
-restaurants.onclick = function() {revealRemHalls()};
+placeOrder.onclick = function() {revealRemove(orderBox)};  //shows and hides order box
+restaurants.onclick = function() {revealRemove(hallList)};  //shows and hides restaurants in restaurant selection tab
 
 
 
