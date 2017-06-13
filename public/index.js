7b7b0fbecf1f35c3c5adc0b14d30a628aca615db
MagicalEight <matthew_ruder@outlook.com>
2017-06-12 17:04:33 -0700

    added hide/show feature

--- public/index.js
+++ public/index.js
@@ -3,6 +3,8 @@ var orderData = document.getElementById("ord");
 var restaurants = document.getElementById("restaurants");
 var hallList = document.getElementsByClassName("navsub")[0];
 var orderBox = document.getElementById("order-box");
+var expandBox = document.getElementsByClassName("rest-text");
+var expandList = document.getElementsByClassName("rest-items");
 hallList.style.display = "none";
 orderBox.style.display = "none";
 
@@ -17,6 +19,24 @@ function revealRemove(ele) {
 orderData.onclick = function() {revealRemove(orderBox)};  //shows and hides order box
 restaurants.onclick = function() {revealRemove(hallList)};  //shows and hides restaurants in restaurant selection tab
 
+function expand(num){
+	if (expandList[num].style.display === "none"){
+		expandList[num].style.display = "block";
+		expandBox[num].style.borderBottomLeftRadius = "0px";
+		expandBox[num].style.borderBottomRightRadius = "0px";
+	}else{
+		expandList[num].style.display = "none";
+		expandBox[num].style.borderBottomLeftRadius = "30px";
+		expandBox[num].style.borderBottomRightRadius = "30px";
+	}
+}
+
+for(var i=0; i<expandBox.length; i++)
+{
+	(function(i){
+		expandBox[i].onclick = function() {expand(i)};
+	})(i);
+}
 //This function places the order by turning the strings into a JSON object
 //for later use.
 
